83596cd6cd340e5f17b901c4f2b04ed8bbe16cbd
jseulloa <julloa@jdc.edu.co>
2024-07-25 11:53:48 -0500

    Failed Register log

--- app/type/dao/typeDao.js
+++ app/type/dao/typeDao.js
@@ -20,6 +20,7 @@ class TypeDao {
                 .then((response) => {
                 res.status(200).json({ message: "Inset Type", _id: response._id });
             }).catch((err) => {
+                console.log(err);
                 res.status(400).json({ message: " Failed register Type" });
             });
         });
